f8fbcd0cf8f786bc78eba800d2e5e4e48de5076d
aka <zhangjake0815@gmail.com>
2022-09-15 18:58:30 +0800

    init

---
+++ test/test_number.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "number.h"
+
+TEST(HELLO, NUMB){
+    EXPECT_EQ(add(1,2),3);
+}
+
+// int a = add(1,2);
+// int main(){
+//     int c = a+1;
+//     return c;
+// }
